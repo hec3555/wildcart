d3d732ccc43d0bc57d8b1fcacbc009c681f0b3ac
a004631408j <a004631408j@s09-pc16>
2018-12-07 20:53:17 +0100

    Edit de factura funcionando (con modal, pero sin implementar fechas).
    Cuando se crea la factura YA SI te lleva a las facturas del usuario
    (pero no a esa factura en concreto, meditar que opcion es mejor).
    
    Falta hacer bien:
    - Factura (fechas, modal y diseño).
    - Lineas (modales y diseño).
    - Cambiar diseño de todos los plists (maestros/esclavos, etc.).
    - Hacer maestro/esclavo de tipo usuario y tipo producto.

--- src/main/webapp/js/app/factura/edit.js
+++ src/main/webapp/js/app/factura/edit.js
@@ -1,11 +1,11 @@
 'use strict';
 
-moduleFactura.controller('facturaEditController', ['$scope', '$http', '$location', 'toolService', '$routeParams', 
+moduleFactura.controller('facturaEditController', ['$scope', '$http', '$location', 'toolService', '$routeParams',
     function ($scope, $http, $location, toolService, $routeParams) {
         $scope.id = $routeParams.id;
         $scope.myDate = new Date();
         $scope.ob = "factura";
-        
+
         $http({
             method: 'GET',
             url: '/json?ob=' + $scope.ob + '&op=get&id=' + $scope.id
@@ -32,12 +32,39 @@ moduleFactura.controller('facturaEditController', ['$scope', '$http', '$location
                 params: {json: JSON.stringify(json)}
             }).then(function (response) {
                 $scope.status = response.status;
-                $scope.mensaje = true;
+                $scope.mensajeOK = true;
             }, function (response) {
                 $scope.ajaxDatoFactura = response.data.message || 'Request failed';
                 $scope.status = response.status;
             });
         };
 
+
+
+        $scope.usuarioRefresh = function (quiensoy, consulta) {
+            var form = quiensoy;
+            if (consulta) {
+                $http({
+                    method: 'GET',
+                    //withCredentials: true,
+                    url: 'json?ob=usuario&op=get&id=' + $scope.ajaxDatoFactura.obj_Usuario.id
+                }).then(function (response) {
+                    if (response.data.message != null) {
+                        form.userForm.obj_usuario.$setValidity('valid', true);
+                        $scope.ajaxDatoFactura.obj_Usuario = response.data.message;
+                    } else {
+                        form.userForm.obj_usuario.$setValidity('valid', false);
+                        $scope.ajaxDatoFactura.obj_Usuario.nombre = "Error al acceder al usuario";
+                        $scope.ajaxDatoFactura.obj_Usuario.ape1 = "";
+                    }
+                }, function (response) {
+                    form.userForm.obj_usuario.$setValidity('valid', false);
+                    $scope.ajaxDatoFactura.obj_Usuario.nombre = "Error al acceder al usuario";
+                    $scope.ajaxDatoFactura.obj_Usuario.ape1 = "";
+                });
+            } else {
+                form.userForm.obj_usuario.$setValidity('valid', true);
+            }
+        };
 
     }]);
