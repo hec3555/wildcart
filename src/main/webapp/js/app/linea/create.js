3052a027ebf6cdf4b5ff17a939baa7762626d4e6
a004631408j <a004631408j@s09-pc16>
2018-12-13 20:56:44 +0100

    Descripcion extensa
    En proceso del crear lineas para una factura determinada.
    
    Hecho:
    
     - Manejo de fechas (el datepicker me sale en rojo, mirar bien la 
        validacion de errores. Me ha obligado a bajar una version de angular,
        buscar opciones que no me obliguen a eso o arreglar lo rojo).
     - Si un maestro/esclavo tiene 0, que el 0 te lleve al create (solo 
        hecho en plist de usuario, si tiene 0 facturas, el 0 lleva a crear
        una factura para ese usuario, falta hacerlo en los demas).
     - En los modales, cuando hay un error, el enlace sigue siendo enlace y
        te lleva a un view vacio, eso arreglado en factura/create, si hay 
        error, el enlace se esconde y solo se muestra mensaje de error.
    
    
    
    
    Falta hacer:
     - Cuando se añade maximo de existencias de un producto al carrito, hacer
        algo.
     - Subir imagenes y mostrarlas.
     - Generar PDF de la factura a comprar.
     - Gestion de usuarios (admin, cliente, etc.).
     - Maestros/esclavos, num de usuarios que tiene un tipo, y num de
        productos que tiene un tipo.

--- src/main/webapp/js/app/linea/create.js
+++ src/main/webapp/js/app/linea/create.js
@@ -2,16 +2,44 @@
 
 moduleLinea.controller('lineaCreateController', ['$scope', '$http', '$location', 'toolService', '$routeParams', 
     function ($scope, $http, $location, toolService, $routeParams) {
-        $scope.id = $routeParams.id;
-        $scope.myDate = new Date();
-        $scope.ob = "linea";
         
+        $scope.ob = "linea";
+
+        $scope.idFac = $routeParams.id;
+
+        $scope.ajaxDatoLinea = {
+            id: null,
+            cantidad: null,
+            obj_producto: {id: null},
+            obj_factura: {id: null}
+        };
+        
+        
+        if($routeParams.id !== "undefined" && $routeParams.id !== "" && $routeParams.id){
+            $scope.ajaxDatoLinea.obj_factura.id = $scope.idFac;
+            $scope.idFacExist = true;
+            $http({
+                    method: 'GET',
+                    //withCredentials: true,
+                    url: 'json?ob=factura&op=get&id=' + $scope.ajaxDatoLinea.obj_factura.id
+                }).then(function (response) {
+                    if (response.data.message !== null) {
+                        $scope.ajaxDatoLinea.obj_factura = response.data.message;
+                    } else {
+                        $scope.ajaxDatoLinea.obj_factura.id = "Error al acceder a la factura";
+                    }
+                }, function (response) {
+                    $scope.ajaxDatoLinea.obj_factura.id = "Error al acceder a la factura";
+                });
+        }else{
+            $scope.idFacExist = false;
+        }
         $scope.guardar = function () {
             var json = {
                 id: null,
-//                cantidad : ,
-//                id_producto : ,
-//                id_factura : 
+                cantidad: $scope.cantidad,
+                id_producto: $scope.ajaxDatoLinea.obj_producto.id,
+                id_factura: $scope.ajaxDatoLinea.obj_factura.id
             };
             $http({
                 method: 'GET',
@@ -20,14 +48,38 @@ moduleLinea.controller('lineaCreateController', ['$scope', '$http', '$location',
                 params: {json: JSON.stringify(json)}
             }).then(function (response) {
                 $scope.status = response.status;
-                $scope.mensaje = true;
+                $scope.mensajeOK = true;
             }, function (response) {
-                $scope.ajaxDatoLinea = response.data.message || 'Request failed';
+                $scope.ajaxDatoLinea = response.message;
                 $scope.status = response.status;
             });
         };
 
 
-        $scope.isActive = toolService.isActive;
+        $scope.productoRefresh = function (quiensoy, consulta) {
+            var form = quiensoy;
+            if (consulta) {
+                $http({
+                    method: 'GET',
+                    //withCredentials: true,
+                    url: 'json?ob=producto&op=get&id=' + $scope.ajaxDatoLinea.obj_producto.id
+                }).then(function (response) {
+                    if (response.data.message !== null) {
+                        form.userForm.obj_producto.$setValidity('valid', true);
+                        $scope.ajaxDatoLinea.obj_producto = response.data.message;
+                    } else {
+                        form.userForm.obj_producto.$setValidity('valid', false);
+                        $scope.ajaxDatoLinea.obj_producto.id = "Error al acceder al usuario";
+                    }
+                }, function (response) {
+                    form.userForm.obj_producto.$setValidity('valid', false);
+                    $scope.ajaxDatoLinea.obj_producto.id = "Error al acceder al usuario";
+                });
+            } else {
+                form.userForm.obj_producto.$setValidity('valid', true);
+            }
+        };
+
+
 
     }]);
